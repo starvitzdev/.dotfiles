278fff885ce30a361d10b621f585c5f4687c49dc
starvitz <starvitz@pm.me>
2025-01-12 16:04:24 +0100

    New settings added

---
+++ bin/restore_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd
+cp -Tr ~/.dotfiles/waybar ~/.config/waybar 
+cp -Tr ~/.dotfiles/nvim ~/.config/nvim
+cp -Tr ~/.dotfiles/sway ~/.config/sway
+cp -Tr ~/.dotfiles/fuzzel ~/.config/fuzzel
+sudo cp -Tr ~/.dotfiles/keyd /etc/keyd
+cp -Tr ~/.dotfiles/.wezterm.lua ~/.wezterm.lua
+cp -Tr ~/.dotfiles/.zshrc ~/.zshrc
+cp -Tr ~/.dotfiles/wallpaper ~/wallpaper
+sudo cp -Tr ~/.dotfiles/bin ~/.local/bin
+cp -Tr ~/.dotfiles/.oh-my-posh ~/.oh-my-posh
